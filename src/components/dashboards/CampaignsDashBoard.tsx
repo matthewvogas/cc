c651c63ffeac21bc3aab5ae054036afc64a58539
Mattheo Guillen <matthew.guillen@golabstech.com>
2023-06-14 23:16:03 +0000

    React/improvements

--- src/components/dashboards/CampaignsDashBoard.tsx
+++ src/components/dashboards/CampaignsDashBoard.tsx
@@ -11,6 +11,7 @@ import axios from 'axios'
 import { Client, campaign } from '@prisma/client'
 import useCampaigns from '@/hooks/useCampaigns'
 import useClients from '@/hooks/useClients'
+import TitlePage from '../titlePage'
 
 export default function CampaignsDashBoard({
   campaignsFallback,
@@ -49,88 +50,15 @@ export default function CampaignsDashBoard({
 
   return (
     <>
-      <div className='flex flex-col gap-4 bg-white'>
-        <div className='w-full pt-20 '>
-          <div className='mx-auto w-full justify-between px-4 md:px-12'>
-            <div className='flex w-full  flex-row justify-between'>
-              <h3
-                className={`pb-8 align-middle text-2xl font-semibold text-gray-800`}>
-                Campaigns
-              </h3>
-              <div className={`flex ${ptMono.className}`}>
-                {/* <div className='dropdown-end dropdown '>
-                    <label
-                      tabIndex={0}
-                      className={`text-back mx-2 flex cursor-pointer rounded-full border-2 border-rose-100 bg-transparent px-8 py-2.5 text-sm  font-medium hover:bg-rose-100`}>
-                      filter
-                    </label>
-                    <div
-                      tabIndex={0}
-                      className='dropdown-content menu rounded-box mr-4 mt-2 w-auto border-2 border-gray-100 bg-white p-2'>
-                      <div className='m-4 flex flex-col gap-5'>
-                        <div className='flex flex-row gap-2'>
-                          <button className='rounded-full bg-beigeFirst px-6 py-2.5  hover:bg-beigeSelected focus:bg-beigeSelected'>
-                            newer
-                          </button>
-                          <button className='rounded-full bg-beigeFirst px-6 py-2.5  hover:bg-beigeSelected focus:bg-beigeSelected'>
-                            older
-                          </button>
-                        </div>
-                        <div className='w-full'>
-                          <label className='' htmlFor=''>
-                            Client
-                          </label>
-                          <select
-                            id='countries'
-                            className='mt-2 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-rose-400 focus:ring-rose-300'>
-                            <option disabled>Choose a client</option>
-                            <option value={-1}>No Client</option>
-                            {clients.map((client, index) => (
-                              <option value={client.id} key={index}>
-                                {client.name}
-                              </option>
-                            ))}
-                          </select>
-                        </div>
-                      </div>
-                    </div>
-                  </div> */}
-                <div className='dropdown-end dropdown cursor-pointer'>
-                  <label
-                    tabIndex={0}
-                    className={`text-back mx-2  flex cursor-pointer items-center justify-center rounded-full border-2 border-rose-100 bg-transparent px-8 py-2.5 text-lg  font-medium hover:bg-rose-100`}>
-                    add new
-                    <svg
-                      fill='none'
-                      viewBox='0 0 26 26'
-                      strokeWidth='1.5'
-                      stroke='currentColor'
-                      className='text-midBlack ml-4 h-5 w-5'>
-                      <path
-                        strokeLinecap='round'
-                        strokeLinejoin='round'
-                        d='M19.5 8.25l-7.5 7.5-7.5-7.5'
-                      />
-                    </svg>
-                  </label>
-                  <ul
-                    tabIndex={0}
-                    className='dropdown-content menu rounded-box mr-4 mt-2 flex w-auto gap-2 border-2 border-gray-100 bg-white p-2'>
-                    <button
-                      className=' rounded-full bg-beigeFirst px-6 py-2.5  hover:bg-beigeSelected focus:bg-beigeSelected'
-                      onClick={() => setIsOpen(true)}>
-                      manual
-                    </button>
-                    {/* <button className=' rounded-full bg-beigeFirst px-6 py-2.5  hover:bg-beigeSelected focus:bg-beigeSelected'>
-                        hashtag
-                      </button> */}
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className='divider' />
-        </div>
+      <TitlePage
+        title={'Campaigns'}
+        moduleText={'campaigns'}
+        client={''}
+        createClient={null}
+        createCampaign={setIsOpen}
+      />
+
+      <div className='flex flex-col gap-4 bg-white pt-12'>
         <div className='flex flex-wrap gap-6 md:px-12'>
           {campaigns.map((card: any, index: any) => (
             <Link
