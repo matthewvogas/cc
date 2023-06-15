c375ebeaf3d891e0b2cad8b1c46f57b85d2da31e
Mattheo Guillen <matthew.guillen@golabstech.com>
2023-06-15 14:44:35 +0000

    Overflow, Scroll bug solved

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -4,7 +4,7 @@ import AuthProvider from '@/providers/AuthProvider'
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <AuthProvider>
-      <div className='flex'>
+      <div className='flex overflow-clip'>
         <Sidebar />
         <main className='flex-1'>{children}</main>
       </div>
