1f5e58a0a602e4db581f7db55d165cc9631171d7
DewinU <umana.dewin@gmail.com>
2023-06-28 14:41:34 -0600

    Linting

--- src/app/api/instagram/ig_hashtag_search/route.ts
+++ src/app/api/instagram/ig_hashtag_search/route.ts
@@ -32,9 +32,14 @@ export async function GET(req: Request) {
     const igId = await hashtagSearch(hashtag)
     if (!igId) throw new Error('Hashtag not found')
 
-    const url: URL = new URL(`https://graph.facebook.com/v16.0/${igId}/top_media`)
+    const url: URL = new URL(
+      `https://graph.facebook.com/v16.0/${igId}/top_media`,
+    )
     url.searchParams.append('user_id', '17841447832021774')
-    url.searchParams.append('fields', 'id,caption,media_type,comments_count,like_count,media_url,permalink')
+    url.searchParams.append(
+      'fields',
+      'id,caption,media_type,comments_count,like_count,media_url,permalink',
+    )
     url.searchParams.append(
       'access_token',
       'EAAR5U6jL0JcBAFG9unBeqjKdZBVCaBs2veaRw4dChUYZCweyVV47c1J96F5ZAVZCywvpaeVBaNhXpXwN1CpBH74GW1BoLhpX2kRuxht5uSmVj7u9wJ6mLtAdlprBxGNXf16elTuo7VPfM9ZCd1BJHJ3EwGcLmacgwumCa2ZCZAILZAV4O98Y6Eu9',
@@ -42,7 +47,6 @@ export async function GET(req: Request) {
 
     const res = await fetch(url).then(res => res.json())
     return NextResponse.json(res)
-
   } catch (e: any) {
     return NextResponse.json({ message: e.message })
   }
