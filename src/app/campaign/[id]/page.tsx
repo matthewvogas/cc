10281daf20c8f7ea10e0f0d691523907e750a762
DewinU <umana.dewin@gmail.com>
2023-08-16 16:46:52 -0600

    Errors

--- src/app/campaign/[id]/page.tsx
+++ src/app/campaign/[id]/page.tsx
@@ -14,10 +14,7 @@ export default async function shareCampaign({
   params: { id: number }
 }) {
   const { id } = params
-
-  const session = await getServerSession(authOptions)
-  const creators = (await CreatorsService.findMany(
-    session!.user.id,
+  const creators = (await CreatorsService.findByCampaignId(id
   )) as CreatorsByCampaignRes[]
 
   const campaign = (await CampaignsService.findUnique(id)) as CampaignRes
@@ -29,7 +26,8 @@ export default async function shareCampaign({
         <SharedCampaign
           campaign={campaign}
           posts={posts}
-          creators={creators}></SharedCampaign>
+          creators={creators}
+          ></SharedCampaign>
       ) : (
         <div className='flex justify-center items-center h-screen bg-[#F3F0EC]'>
           <h3 className='text-lg px-6 py-3  bg-[#8a7356] text-white shadow-xl  rounded-xl'>
