b0835cc0386a5d98dc5f4d619ba12e34c250eabe
Emmanuel CM <emmanuel.cerdas@golabstech.com>
2024-01-03 14:10:50 -0600

    added modal in creators

--- src/components/modals/agency/addCreators.tsx
+++ src/components/modals/agency/addCreators.tsx
@@ -39,6 +39,14 @@ export default function AddCreators({ userCreators, session }: Props) {
     return creatorNameMatches
   })
 
+  const [codeToCopy, setcodeToCopy] = React.useState('');
+
+  const html =
+    '<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <meta http-equiv="X-UA-Compatible"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <title>Stats</title> </head> <body>' +
+    codeToShareInvite + '</body> </html>';
+
+  const iframe = codeToShareInvite;
+
   const sendInvite = async () => {
     try {
       const res = await fetch('/api/invite', {
@@ -53,7 +61,7 @@ export default function AddCreators({ userCreators, session }: Props) {
       })
 
       if (res.status === 200) console.log(res.status)
-    } catch (error: any) {}
+    } catch (error: any) { }
 
     setIsOpen(false)
   }
@@ -114,6 +122,19 @@ export default function AddCreators({ userCreators, session }: Props) {
     }
   }
 
+  const copiarTexto = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text)
+      const copiadoModal = document.getElementById('copiadoModal')
+      copiadoModal?.classList.remove('hidden')
+      setTimeout(() => {
+        copiadoModal?.classList.add('hidden')
+      }, 1000)
+    } catch (err) {
+      console.error('Could not copy text: ', err)
+    }
+  }
+
   return (
     <>
       <button
@@ -147,8 +168,7 @@ export default function AddCreators({ userCreators, session }: Props) {
                 className={`flex flex-row items-center justify-center gap-6 ${ptMono.className}`}>
                 <Tab
                   className={({ selected }) =>
-                    ` rounded-3xl border-2 border-primary px-12 py-2 ${
-                      selected ? 'bg-primary' : ''
+                    ` rounded-3xl border-2 border-primary px-12 py-2 ${selected ? 'bg-primary' : ''
                     }`
                   }>
                   add manually
@@ -156,8 +176,7 @@ export default function AddCreators({ userCreators, session }: Props) {
 
                 <Tab
                   className={({ selected }) =>
-                    `rounded-3xl border-2 border-primary px-8  py-2 ${
-                      selected ? 'bg-primary' : ''
+                    `rounded-3xl border-2 border-primary px-8  py-2 ${selected ? 'bg-primary' : ''
                     }`
                   }>
                   invite to connect
@@ -304,11 +323,76 @@ export default function AddCreators({ userCreators, session }: Props) {
                         Have your own site you want to invite your creators to
                         sign up for this campaign from? Copy this embed code.
                       </label>
-                      <button
-                        onClick={copyToClipboardCode}
-                        className={`text-sm ml-6 w-80 rounded-full border border-[#FACEBC] active:bg-opacity-10 px-8 focus:border-[#c98e77] hover:border-[#eeaf97] active:bg-rose-300 ${ptMono.className}`}>
-                        embed a form
-                      </button>
+                      <div>
+                        <button
+                          className={`text-sm ml-6 w-40 rounded-full border border-[#FACEBC] active:bg-opacity-10 px-8 focus:border-[#c98e77] hover:border-[#eeaf97] active:bg-rose-300 ${ptMono.className}`}>
+                          <label htmlFor='my-modal-3'> embed a form</label>
+                        </button>
+                        <input type='checkbox' id='my-modal-3' className='  modal-toggle' />
+                        <div className='modal '>
+                          <div className='modal-box relative flex max-w-max flex-col justify-start overflow-hidden rounded-xl bg-white  p-0'>
+                            <label
+                              htmlFor='my-modal-3'
+                              className='absolute right-4 top-2 cursor-pointer text-lg text-white'>
+                              ✕
+                            </label>
+
+                            <div className='px-10 py-8'>
+                              <h3 className='text-xl font-bold'>
+                                Embed your total live stats on to a webpage
+                              </h3>
+
+                              <div className={`w-full justify-start `}>
+                                <p className={` pb-6 text-xs text-[#7F7F7F] mt-2`}>
+                                  Copy and paste a code below. You can manage styles via CSS.
+                                </p>
+
+                                <div className='flex gap-6'>
+                                  <div className=' flex w-56 text-sm flex-col gap-3'>
+                                    <button
+                                      onClick={() => setcodeToCopy(html)}
+                                      className='w-full rounded-full border border-[#FACEBC] px-8 py-2 hover:bg-[#FACEBC]'>
+                                      HTML + CSS
+                                    </button>
+                                    <button
+                                      onClick={() => setcodeToCopy(iframe)}
+                                      className='w-full rounded-full border border-[#FACEBC] px-8 py-2 hover:bg-[#FACEBC]'>
+                                      iframe
+                                    </button>
+                                  </div>
+
+                                  <div
+                                    className={`w-full rounded-xl border border-[#7F7F7F] p-6 ${ptMono.className}`}>
+                                    <textarea
+                                      value={codeToCopy}
+                                      readOnly
+                                      placeholder='https://'
+                                      className='h-[124px] min-w-full resize-none text-gray-600 outline-none'
+                                    />
+                                    <div
+                                      id='copiadoModal'
+                                      className={` ${inter.className} fixed top-1/2 left-1/2 transform -translate-x-1/2  -translate-y-1/2 px-4 py-2 border border-[#b6fcdb] bg-[#e9faf2] bg-opacity-90 text-sm rounded-md z-50 hidden`}>
+                                      link copied successfully!
+                                    </div>
+                                  </div>
+                                </div>
+
+                                <hr className=' h-px bg-gray-200 my-8'></hr>
+
+                                <div className='text-right'>
+                                  <button
+                                    onClick={() => {
+                                      copiarTexto(codeToCopy)
+                                    }}
+                                    className={`${ptMono.className} rounded-lg bg-[#D3F0E2] px-8 py-2`}>
+                                    {'copy </>'}
+                                  </button>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                     {isCopied && (
                       <div className='shadow-lg fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white border border-gray-300 px-4 py-2 rounded-md'>
@@ -322,6 +406,8 @@ export default function AddCreators({ userCreators, session }: Props) {
           </Dialog.Panel>
         </div>
       </Dialog>
+
+
     </>
   )
 }
